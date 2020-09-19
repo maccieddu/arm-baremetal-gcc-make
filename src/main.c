c62671200f0a369b5694cecfeb281d3aab0a5a8e
marcello <marcello@email.com>
2020-09-19 22:53:16 +0200

    !!! Main update !!! - Removed static keyword to test the placement of arr array in the data section

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ float i = 1.0f;
 float j = 2.0f;
 float k = 0.0f;
 
-static int arr[] = { 1, 10, 4, 5, 6, 7 };
+int arr[] = { 1, 10, 4, 5, 6, 7 };
 
 const int c = 1;
 const int cc = 2;
@@ -18,5 +18,6 @@ void main(void)
     while(1)
     {
         k = i / j;
+        k = add(arr[0], arr[1]);
     }
 }
